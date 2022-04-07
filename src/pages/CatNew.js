6e49b0838d1274a2893042890cfb7ecdcb3376d1
Valeriano-WebDev <Valeriano-WebDev@gmail.com>
2022-04-07 10:55:12 -0700

    create functionality completed

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,12 +1,56 @@
 import React, {Component} from "react";
+import { Button, Form, FormGroup, Input, Label} from 'reactstrap'
+import { Redirect } from 'react-router-dom'
 
 export default class CatNew extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            newCat: {
+                name: "",
+                age: "",
+                enjoys: "",
+                image: ""
+            },
+            submitted: false
+        }
+    }
+
+    handleSubmit = () => {
+        this.props.createCat(this.state.newCat)
+        this.setState({submitted: true})
+    }
+
+    handleChange = (e) => {
+        let { newCat } = this.state
+        newCat[e.target.name] = e.target.value
+        this.setState({newCat: newCat})
+    }
+
     render() {
         return(
             <>
-                <h1>New </h1>
-            </>
+                <h1>Add a new Fur Friend!</h1>
+                <Form>
+                    <FormGroup>
+                        <Label>Name</Label>
+                        <Input type="text" name="name" onChange={this.handleChange}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Age</Label>
+                        <Input type="text" name="age" onChange={this.handleChange}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Enjoys</Label>
+                        <Input type="text" name="enjoys" onChange={this.handleChange}/>
+                    </FormGroup>
+                    <Button name="submit" onClick={this.handleSubmit}>
+                        Create a New Profile
+                    </Button>
+                    {this.state.submitted && <Redirect to="/catindex" />}
+                </Form>
 
+            </>
         )
     }
 }
